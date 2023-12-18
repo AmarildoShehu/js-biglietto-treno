69c298305ed69fd9961ac9251b22f037a5403e18
Amarildo <amarildos@outloo.it>
2023-12-18 15:44:19 +0100

    applied discount

--- js/script.js
+++ js/script.js
@@ -14,3 +14,13 @@ const prezzoPerKm = 0.21;
 const prezzoBiglietto = kmDaPercorrere * prezzoPerKm;
 
 console.log(prezzoBiglietto);
+
+//Applico sconto sull'età.
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+if (etàPasseggero < 18){
+    prezzoBiglietto *= 0.8;
+} else if (etàPasseggero >= 65){
+    prezzoBiglietto *= 0.6;
+}

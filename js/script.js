665491200eb5b8f48cd808f7b9263f166e96d5d9
Amarildo <amarildos@outloo.it>
2023-12-18 15:24:38 +0100

    added const km and age

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('JS OK');
+
+
+//Chiedo quanti km vuole percorrere e quanti anni ha.
+
+const kmDaPercorrere = prompt('inserisci km da percorrere', '25');
+const etàPasseggero = prompt('inserisci la tua età', '31');
+
+console.log( kmDaPercorrere, etàPasseggero);

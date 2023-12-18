741751ab70de103924dcc9beadf35150cd2e9eb2
Amarildo <amarildos@outloo.it>
2023-12-18 16:22:21 +0100

    show ticket cost

--- js/script.js
+++ js/script.js
@@ -3,24 +3,31 @@ console.log('JS OK');
 
 //Chiedo quanti km vuole percorrere e quanti anni ha.
 
-const kmDaPercorrere = prompt('inserisci km da percorrere', '25');
-const etàPasseggero = prompt('inserisci la tua età', '31');
+let KmDaPercorrere = prompt("Quanti chilometri vuoi percorrere?", '21');
+let etàPasseggero = prompt("Quanti anni hai?" );
 
-console.log( kmDaPercorrere, etàPasseggero);
+console.log( KmDaPercorrere, etàPasseggero);
 
 //Calcolo il prezzo del biglietto per km percorsi.
 
-const prezzoPerKm = 0.21;
-const prezzoBiglietto = kmDaPercorrere * prezzoPerKm;
-
-console.log(prezzoBiglietto);
+const prezzoAKm = 0.21;
+let prezzoTotale = KmDaPercorrere * prezzoAKm;
 
 //Applico sconto sull'età.
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-if (etàPasseggero < 18){
-    prezzoBiglietto *= 0.8;
-} else if (etàPasseggero >= 65){
-    prezzoBiglietto *= 0.6;
+if (etàPasseggero < 18) {
+    prezzoTotale *= 0.8;
+} else if (etàPasseggero >= 65) {
+    prezzoTotale *= 0.6;
 }
+
+//prezzo finale con massimo due decimali
+prezzoTotale = prezzoTotale.toFixed(2);
+
+// Mostra il prezzo finale all'utente
+console.log(`Il prezzo del viaggio è di € ${prezzoTotale}`);
+
+document.getElementById("prezzototale").innerText = "il costo del Boglietto é " + prezzoTotale ;
+

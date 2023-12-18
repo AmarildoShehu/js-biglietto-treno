91336501736f2fbb42109f8795d6a1a8d534cb7a
Amarildo <amarildos@outloo.it>
2023-12-18 15:31:51 +0100

    price calculated

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ const kmDaPercorrere = prompt('inserisci km da percorrere', '25');
 const etàPasseggero = prompt('inserisci la tua età', '31');
 
 console.log( kmDaPercorrere, etàPasseggero);
+
+//Calcolo il prezzo del biglietto per km percorsi.
+
+const prezzoPerKm = 0.21;
+const prezzoBiglietto = kmDaPercorrere * prezzoPerKm;
+
+console.log(prezzoBiglietto);
